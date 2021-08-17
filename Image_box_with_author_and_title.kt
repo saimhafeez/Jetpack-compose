55bf85c5674c1bed55b811f59e33a44caa0cdf75
Saim Hafeez <73340622+saimhafeez20@users.noreply.github.com>
2021-08-17 13:57:01 +0500

    Create Image_box_with_author_and_title.kt
    For Detailed Explanation, Visit saimhafeez.com
    or go to

---
+++ Image_box_with_author_and_title.kt
@@ -0,0 +1,75 @@
+@Composable
+fun Image_Card(
+    /* Parameters */
+    cover_painter: Painter,
+    author_painter: Painter,
+    title: String)
+{
+
+    Card(modifier = Modifier
+            /* Card Style */
+        .fillMaxWidth(0.5f)
+        .height(250.dp)
+        .padding(5.dp),
+        elevation = 8.dp
+    ) {
+        Column(modifier = Modifier
+            /* Card Will Contain One Column and Three Rows*/
+            .fillMaxSize(),
+        verticalArrangement = Arrangement.Top) {
+
+            /* Row #1 --> Cover Image */
+            Row(modifier = Modifier
+                .fillMaxWidth()
+                .fillMaxHeight(0.5f)
+                .drawWithContent {
+                    drawContent()
+                    val strokeWidth = Stroke.DefaultMiter
+                    val y = size.height // - strokeWidth
+                    drawLine(
+                        brush = SolidColor(Color.Black),
+                        strokeWidth = strokeWidth,
+                        cap = StrokeCap.Square,
+                        start = Offset.Zero.copy(y = y),
+                        end = Offset(x = size.width, y = y)
+                    )
+                },
+            horizontalArrangement = Arrangement.Start) {
+                Image(painter = cover_painter, contentDescription = "Cover Image", contentScale = ContentScale.Crop)
+            }
+
+            /* Row #2 --> Author Image */
+            Row(modifier = Modifier
+                .fillMaxWidth(),
+                //.offset(0.dp, (-32).dp),
+                horizontalArrangement = Arrangement.Center) {
+                Box(modifier = Modifier
+                    .size(64.dp)
+                    .offset(0.dp, (-32).dp)
+                    .border(2.dp, Color.Black, shape = CircleShape)
+                    .clip(CircleShape)){
+                    Image(painter = author_painter, contentDescription = "Author Image", contentScale = ContentScale.Crop)
+                }
+
+            }
+
+            /* Row #3 --> Title/Heading/Description */
+            Row(
+                modifier = Modifier
+                .fillMaxWidth(),
+                horizontalArrangement = Arrangement.Start)
+            {
+                Box(modifier = Modifier
+                    .offset(0.dp, (-25).dp)
+                    .padding(10.dp, 0.dp, 10.dp, 0.dp)
+                    .verticalScroll(rememberScrollState())
+                ){
+                    Text(text = title,
+                        style = TextStyle(Color.Black, fontSize = 12.sp)
+                    )
+                }
+            }
+
+        }
+    }
+}
